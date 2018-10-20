92d0efda7d3a7bea9821a559a184d096845f1988
MCrank <marco_mcse@hotmail.com>
2018-10-20 10:37:50 -0500

    Added moment.js and helper functions

--- javascripts/helpers/utils.js
+++ javascripts/helpers/utils.js
@@ -1,1 +1,12 @@
 console.log('Utils.js');
+
+const printToDom = (stringToPrint, elementId) => {
+  const selectedDiv = document.getElementById(elementId);
+  selectedDiv.innerHTML = stringToPrint;
+};
+
+const getCurrentTime = () => {
+  return moment().format();
+};
+
+export { printToDom, getCurrentTime };
